93a199ce1befb89e73c6e17e3fa0d9c2efef0a47
juandejunin <juandejunin75@gmail.com>
2024-06-19 09:10:31 +0200

    feat: add ffmpeg to the app so that it works on any pc regardless of whether it has ffmpeg installed or not

--- main.js
+++ main.js
@@ -1,42 +1,47 @@
 // main.js
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const electron = require('electron');
-const appDownload = require('./appDownload');
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const ffmpeg = require('fluent-ffmpeg')
+const ffmpegPath = require('ffmpeg-static')
 
-let mainWindow;
+// Configura la ruta de ffmpeg
+ffmpeg.setFfmpegPath(ffmpegPath)
 
-function createWindow() {
+console.log('Ruta de ffmpeg-static:', ffmpegPath)
+
+const appDownload = require('./appDownload')
+
+let mainWindow
+
+function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    maxHeight:800,
-    fullscreen: false, 
+    maxHeight: 800,
+    fullscreen: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
     }
-  });
+  })
 
-  mainWindow.loadFile('public/index.html');
+  mainWindow.loadFile('public/index.html')
 
   mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+    mainWindow = null
+  })
 }
 
 app.whenReady().then(() => {
- 
-  const { app: appInstance, server } = appDownload();
+  const { app: appInstance, server } = appDownload()
 
-  createWindow();
-});
+  createWindow()
+})
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
-});
+  if (process.platform !== 'darwin') app.quit()
+})
 
 app.on('activate', function () {
-  if (mainWindow === null) createWindow();
-});
-
+  if (mainWindow === null) createWindow()
+})
--- public/downloadScript.js
+++ public/downloadScript.js
@@ -1,33 +1,32 @@
-function clearUrlInput() {
-  document.getElementById('urlInput').value = '';
+function clearUrlInput () {
+  document.getElementById('urlInput').value = ''
 }
 
-function showMessage(message) {
-  alert(message); 
+function showMessage (message) {
+  alert(message)
 }
 
-function sendRequest() {
-  const form = document.getElementById('downloadForm');
-  const formData = new FormData(form);
+function sendRequest () {
+  const form = document.getElementById('downloadForm')
+  const formData = new FormData(form)
 
   fetch('http://localhost:3000/download', {
-      method: 'POST',
-      body: formData
+    method: 'POST',
+    body: formData
   })
-  .then(response => {
+    .then(response => {
       if (!response.ok) {
-          return response.json().then(data => {
-              throw new Error(data.errors ? data.errors[0].msg : 'Error en la red');
-          });
-      }       
-     
-  })
-  .then(data => {
-      showMessage('¡Descarga completa!');
-      clearUrlInput();
-  })
-  .catch(error => {
-      console.error('Error:', error);
-      showMessage(error.message);
-  });
+        return response.json().then(data => {
+          throw new Error(data.errors ? data.errors[0].msg : 'Error en la red')
+        })
+      }
+    })
+    .then(data => {
+      showMessage('¡Descarga completa!')
+      clearUrlInput()
+    })
+    .catch(error => {
+      console.error('Error:', error)
+      showMessage(error.message)
+    })
 }
